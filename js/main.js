81fc00b22a03d935207a8f035a85879370ca289a
Arjun Mahishi <arjun.mahishi@gmail.com>
2017-08-21 15:24:09 +0530

    Added new structure

---
+++ js/main.js
@@ -0,0 +1,29 @@
+function showEvents(){
+    $("#events").show();
+    $("#home").hide();
+    $("#sponcers").hide();
+    $("#team").hide();
+}
+
+function showHome(){
+    $("#events").hide();
+    $("#home").show();
+    $("#sponcers").hide();
+    $("#team").hide();
+}
+
+function showTeam(){
+    $("#events").hide();
+    $("#home").hide();
+    $("#sponcers").show();
+    $("#team").hide();
+}
+
+function showTeam(){
+    $("#events").hide();
+    $("#home").hide();
+    $("#sponcers").hide();
+    $("#team").show();
+}
+
+showHome();
