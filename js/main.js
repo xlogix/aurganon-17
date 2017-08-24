590bb38d77e360f3f1cf281f7a8c1ce3a2191758
Arjun Mahishi <arjun.mahishi@gmail.com>
2017-08-24 14:09:06 +0530

    Added basic regis form

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+// State variables
+var REGIS_VARIABLES = false;
+
 function showEvents(){
     $("#events").show();
     $("#home").hide();
@@ -24,6 +27,38 @@ function showTeam(){
     $("#home").hide();
     $("#sponcers").hide();
     $("#team").show();
+}
+
+function showRegister(){
+    let dialogHTML = `
+		<div class="modal fade" id="regis-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					<h4 class="modal-title" id="myModalLabel">Register</h4>
+				</div>
+				<div class="modal-body">
+                <input class="form-control text-box" type="text" placeholder="Name" />
+                <input class="form-control text-box" type="text" placeholder="Phone no" />
+                <input class="form-control text-box" type="text" placeholder="Email" />
+                <input class="form-control text-box" type="password" placeholder="Password" />
+				</div>
+				<div class="modal-footer">
+					<button id="close-confirm-modal" type="button" class="btn harry-btn modal-btn" 
+					data-dismiss="modal">Close</button>
+					<button type="button" onclick="" class="btn harry-btn modal-btn">Order</button>
+				</div>
+				</div>
+			</div>
+		</div>
+	`;
+	$("#regis-plate").html(dialogHTML);
+	$("#regis-modal").modal("show");
+}
+
+function closeRegister(){
+
 }
 
 showHome();
