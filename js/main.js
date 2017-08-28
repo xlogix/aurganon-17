043380d80fce073629c5a335b36fc09daaae2375
arunsri7 <arun.i1997@gmail.com>
2017-08-28 14:04:49 +0530

    login pop-up

--- js/main.js
+++ js/main.js
@@ -47,7 +47,7 @@ function showRegister(){
 				<div class="modal-footer">
 					<button id="close-confirm-modal" type="button" class="btn harry-btn modal-btn" 
 					data-dismiss="modal">Close</button>
-					<button type="button" onclick="" class="btn harry-btn modal-btn">Order</button>
+					<button type="button" onclick="" class="btn harry-btn modal-btn">Submit</button>
 				</div>
 				</div>
 			</div>
@@ -59,6 +59,31 @@ function showRegister(){
 
 function closeRegister(){
 
+}
+function showLogin(){
+    let dialogHTML = `
+		<div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					<h4 class="modal-title" id="myModalLabel">login</h4>
+				</div>
+				<div class="modal-body">
+                <input class="form-control text-box" type="text" placeholder="Email" />
+                <input class="form-control text-box" type="password" placeholder="Password" />
+				</div>
+				<div class="modal-footer">
+					<button id="close-confirm-modal" type="button" class="btn harry-btn modal-btn" 
+					data-dismiss="modal">Close</button>
+					<button type="button" onclick="" class="btn harry-btn modal-btn">Submit</button>
+				</div>
+				</div>
+			</div>
+		</div>
+	`;
+	$("#login-plate").html(dialogHTML);
+	$("#login-modal").modal("show");
 }
 
 showHome();
